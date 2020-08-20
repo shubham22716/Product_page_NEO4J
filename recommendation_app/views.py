b1ce4d1316451a6c1e9bc01b94057ddd9de95414
shubham22716 <sp22716@gmail.com>
2020-08-20 19:41:10 +0530

    'h'

--- recommendation_app/views.py
+++ recommendation_app/views.py
@@ -110,50 +110,57 @@ def recommendation_algo_one(key):
     product_frame.columns = ['ProductId' ,'ProductName', 'Manufacturer','Style','Finish','Category','Price','Poster']
     product_list = list(product_frame.apply(set_product, axis=1))
     return product_list
-def recommendation_algo_one(key):
-    query = '''MATCH (p:Product)
-        where p.ProductId ="'''+str(key)+'''"
-        WITH p
-        match(p1:Product{SubCategory:p.SubCategory})<-[:HasProduct]-(c:Category) where p<>p1
-        with p,p1,c,apoc.text.sorensenDiceSimilarity(p.ProductName, p1.ProductName) as sim order  by sim desc
-        RETURN  toString(p1.ProductId) as id, p1.ProductName as ProductName, p1.Manufacturer as Manufacturer, p1.Style as Style, p1.Finish as Finish, c.Category as Category,p1.Price as Price, p1.poster as Poster limit 6'''
-
-    product_frame = pd.DataFrame(driver.session().run(query))
-    
-    if(len(product_frame) == 0):
-        return('Empty')
-    
-    product_frame.columns = ['ProductId' ,'ProductName', 'Manufacturer','Style','Finish','Category','Price','Poster']
-    product_list = list(product_frame.apply(set_product, axis=1))
-    return product_list
-
 
 def recommendation_algo_two(key):
-    query = '''MATCH (p:Product)
-        where p.ProductId ="'''+str(key)+'''"
-        WITH p
-        match(p1:Product{SubCategory:p.SubCategory})<-[:HasProduct]-(c:Category) where p<>p1
-        with p,p1,c,apoc.text.sorensenDiceSimilarity(p.ProductName, p1.ProductName) as sim order  by sim desc
-        RETURN  toString(p1.ProductId) as id, p1.ProductName as ProductName, p1.Manufacturer as Manufacturer, p1.Style as Style, p1.Finish as Finish, c.Category as Category,p1.Price as Price, p1.poster as Poster limit 6'''
-
-    product_frame = pd.DataFrame(driver.session().run(query))
+	query = ''' MATCH (p1:Product)
+	where p1.ProductId ="'''+str(key)+'''"
+	MATCH (c:Customer)-[x:InteractsWith]->(p1)
+	where x.Event = 'PURCHASE'
+	with collect(c.CustomerId) as ws, p1
+	unwind ws as ws1
+	MATCH (c1:Customer{CustomerId: ws1})-[x:InteractsWith]->(p:Product)
+	where x.Event = 'PURCHASE'
+	with collect(distinct p) as wsa,  p1
+	MATCH (c2)-[x:InteractsWith]->(p:Product{ProductId: p1.ProductId})
+	where x.Event = 'PRODUCT VIEW' or x.Event  = 'CART ITEM ADDITION' or x.Event = 'WISHLIST'
+	with collect(distinct id(c2)) as targetviews, wsa, p1
+	unwind wsa as w2
+	MATCH (c2)-[x:InteractsWith]->(p:Product{ProductId: w2.ProductId})<-[:HasProduct]-(cat)
+	where x.Event = 'PRODUCT VIEW' or x.Event  = 'CART ITEM ADDITION' or x.Event = 'WISHLIST'
+	with collect(distinct id(c2)) as listitemviews, w2, targetviews, p1, cat
+	with p1 , w2, cat, gds.alpha.similarity.jaccard(targetviews, listitemviews) AS similarity
+	where similarity<1 and similarity>=0.01
+	with p1,w2, similarity, cat
+	ORDER BY similarity DESC
+	with p1, collect([w2,similarity, cat])[..6] as products
+	unwind products as recommendation
+	return toString(recommendation[0].ProductId) as ProductId, recommendation[0].ProductName as ProductName, recommendation[0].Manufacturer as Manufacturer, recommendation[0].Style as Style, recommendation[0].Finish as Finish,recommendation[2].Category as Category, recommendation[0].Price as Price, recommendation[0].poster as Poster'''
+		
+	product_frame = pd.DataFrame(driver.session().run(query))
+	print(product_frame)
     
-    if(len(product_frame) == 0):
-        return('Empty')
+	if(len(product_frame) == 0):
+		return('Empty')
+		
+	product_frame.columns = ['ProductId' ,'ProductName', 'Manufacturer','Style','Finish','Category','Price','Poster']
+	product_list = list(product_frame.apply(set_product, axis=1))
+	return product_list
+		
     
-    product_frame.columns = ['ProductId' ,'ProductName', 'Manufacturer','Style','Finish','Category','Price','Poster']
-    product_list = list(product_frame.apply(set_product, axis=1))
-    return product_list
+	
 
 
 def recommendation_algo_three(key):
     query = '''MATCH (p:Product)
         where p.ProductId ="'''+str(key)+'''"
         WITH p
-        match(p1:Product{SubCategory:p.SubCategory})<-[:HasProduct]-(c:Category) where p<>p1
-        with p,p1,c,apoc.text.sorensenDiceSimilarity(p.ProductName, p1.ProductName) as sim order  by sim desc
-        RETURN  toString(p1.ProductId) as id, p1.ProductName as ProductName, p1.Manufacturer as Manufacturer, p1.Style as Style, p1.Finish as Finish, c.Category as Category,p1.Price as Price, p1.poster as Poster limit 6'''
-
+        match(u)-[x:InteractsWith]->(p) where x.EventScore >= 0.25
+with collect (u) as s
+unwind s as d
+match (d)-[x:InteractsWith]->(p1)<-[:HasProduct]-(c) where x.EventScore >.5
+with c, collect (distinct(p1)) as d 
+unwind  d[..6] as p1
+return toString(p1.ProductId) as id, p1.ProductName as ProductName, p1.Manufacturer as Manufacturer, p1.Style as Style, p1.Finish as Finish, c.Category as Category,p1.Price as Price, p1.poster as Poster limit 6'''
     product_frame = pd.DataFrame(driver.session().run(query))
     
     if(len(product_frame) == 0):
@@ -226,7 +233,7 @@ def detail(request, key):
         'products': set_product(product_frame.loc[0]),
         'algo_one': recommendation_algo_one(key),
         'algo_two': recommendation_algo_one(key),
-        'algo_one': recommendation_algo_one(key),
+        'algo_three': recommendation_algo_three(key),
         'search_key':"",
         'search_type': 'All',
         'radios': 'All Product Manufacturer Category'.split()
